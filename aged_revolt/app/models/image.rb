94fd4b4b42008520e6d64cc672b28e6000e60caa
Daniel Levine <daniel.levine4@gmail.com>
2011-03-04 12:26:35 -0500

    adjusted images

--- aged_revolt/app/models/image.rb
+++ aged_revolt/app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < Asset
   validate :no_attachement_errors
   has_attached_file :attachment,
-                    :styles => { :mini => '48x48>', :small => '210x210>', :product => '240x240>', :large => '600x600>' },
+                    :styles => { :mini => '48x48>', :small => '210x210>', :product => '375x300>', :large => '600x600>' },
                     :default_style => :product,
                     :url => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
